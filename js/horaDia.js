bf4def8cfd24991e56368039e7537b76782a6ee4
david-garcia1402 <davidsgarcia1402@gmail.com>
2025-04-09 12:05:26 -0300

    feat: js first level - goodmorning

---
+++ js/horaDia.js
@@ -0,0 +1,28 @@
+function initialize()
+{
+    timeNow = getHours();
+    defineImage(timeNow);
+}
+
+function defineImage(timeNow)
+{
+    if (timeNow <= 12) {
+        // Texto que falará as horas pro usuário
+        let textTimeNow = window.document.getElementById('nowTime');
+        textTimeNow.innerHTML = `Bom dia! Agora são ${timeNow} horas.`
+
+        let img = window.document.getElementById('circleImg');
+        img.style.backgroundImage = "url('assets/images/goodMorning.jpg')";
+    } 
+}
+
+function getHours()
+{
+    // Pegar hora atual (0 a 23)
+    const data = new Date();
+    let timeNow =  data.getHours();
+
+    return timeNow
+}
+
+initialize();
