29b8fb3a99e6cad2f331d2a6df4c0c8eca475a64
david-garcia1402 <davidsgarcia1402@gmail.com>
2025-04-09 12:26:54 -0300

    feat: conditions to evening and afternoon hours-image

--- js/horaDia.js
+++ js/horaDia.js
@@ -6,14 +6,25 @@ function initialize()
 
 function defineImage(timeNow)
 {
-    if (timeNow <= 12) {
-        // Texto que falará as horas pro usuário
-        let textTimeNow = window.document.getElementById('nowTime');
+    if (timeNow <= 12) { 
+        let textTimeNow = window.document.getElementById('nowTime'); // Texto que falará as horas pro usuário
         textTimeNow.innerHTML = `Bom dia! Agora são ${timeNow} horas.`
 
         let img = window.document.getElementById('circleImg');
         img.style.backgroundImage = "url('assets/images/goodMorning.jpg')";
-    } 
+    } else if (timeNow <= 18) {
+        let textTimeNow = window.document.getElementById('nowTime');
+        textTimeNow.innerHTML = `Boa tarde! Agora são ${timeNow} horas.`
+
+        let img = window.document.getElementById('circleImg');
+        img.style.backgroundImage = "url('assets/images/goodAfternoon.jpg')";
+    } else {
+        let textTimeNow = window.document.getElementById('nowTime');
+        textTimeNow.innerHTML = `Boa noite! Agora são ${timeNow} horas.`
+
+        let img = window.document.getElementById('circleImg');
+        img.style.backgroundImage = "url('assets/images/goodEvening.jpg')";
+    }
 }
 
 function getHours()
